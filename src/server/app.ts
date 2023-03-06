3386a230628d041f641df13828b4bff37f3afe99
Jairo Vidomines <jvidomines@gmail.com>
2023-03-06 23:18:37 +0100

    Enabeling CORS safely

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,20 @@
+import "../loadEnvironment.js";
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 export const app = express();
 
+const allowedCorsOrigin = [
+  process.env.CORS_ALLOWED_ORIGIN_LOCAL!,
+  process.env.CORS_ALLOWED_ORIGIN_PRODUCTION!,
+];
+
+const options: cors.CorsOptions = {
+  origin: allowedCorsOrigin,
+};
+
+app.use(cors(options));
 app.disable("x-powered-by");
+app.use(morgan("dev"));
+app.use(express.json());
