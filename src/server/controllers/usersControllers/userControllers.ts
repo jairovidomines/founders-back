84bf31b1b6c8ca68ba94352ddddcbaf5a61806e5
Jairo Vidomines <jvidomines@gmail.com>
2023-03-07 19:47:29 +0100

    Create loginUser controller

---
+++ src/server/controllers/usersControllers/userControllers.ts
@@ -0,0 +1,51 @@
+import { type Request, type Response, type NextFunction } from "express";
+import { type UserCredentials } from "./types.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { loginUsersErrors } from "../../utils/errors.js";
+import { type CustomJwtPayload } from "../../types.js";
+import User from "../../../database/models/User.js";
+import statusCodes from "../../utils/statusCode.js";
+
+const {
+  success: { okCode },
+} = statusCodes;
+
+export const loginUser = async (
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    UserCredentials
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  const { password, username } = req.body;
+
+  try {
+    const user = await User.findOne({ username }).exec();
+
+    if (!user) {
+      next(loginUsersErrors.userNotFound);
+      return;
+    }
+
+    if (!(await bcrypt.compare(password, password))) {
+      next(loginUsersErrors.wrongPassword);
+      return;
+    }
+
+    const jwtPayload: CustomJwtPayload = {
+      id: user._id.toString(),
+      username,
+    };
+
+    const token = jwt.sign(jwtPayload, process.env.JWT_SECRET!, {
+      expiresIn: "3d",
+    });
+
+    res.status(okCode).json({ token });
+  } catch (error: unknown) {
+    next(error);
+  }
+};
