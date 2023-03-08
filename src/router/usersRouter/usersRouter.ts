62110111a507cea1b7e7202762f175d0367875e8
Jairo Vidomines <jvidomines@gmail.com>
2023-03-08 12:15:10 +0100

    Create userLogin endpoint

---
+++ src/router/usersRouter/usersRouter.ts
@@ -0,0 +1,17 @@
+import { validate } from "express-validation";
+import { Router } from "express";
+import { loginUser } from "../../server/controllers/usersControllers/userControllers.js";
+import routes from "../routes.js";
+import loginUserSchema from "../../schemas/userSchemas.js";
+
+const { login } = routes.users;
+
+const usersRouter = Router();
+
+usersRouter.post(
+  login,
+  validate(loginUserSchema, {}, { abortEarly: false }),
+  loginUser
+);
+
+export default usersRouter;
