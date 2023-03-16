ec92daae9fdd4d3c574f933636375f958c0a706b
Jairo Vidomines <jvidomines@gmail.com>
2023-03-16 19:17:09 +0100

    Add deleteProject endpoint to projectsRouter

--- src/server/routers/projectsRouter/projectsRouter.ts
+++ src/server/routers/projectsRouter/projectsRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  deleteProjects,
   getAllProjects,
   getUserProjects,
 } from "../../controllers/projectsControllers/projectsControllers.js";
@@ -8,10 +9,13 @@ import routes from "../routes.js";
 
 const { projects } = routes.projects;
 const { myProjects } = routes.myProjects;
+const { deleteProject } = routes.deleteProject;
+const { id } = routes.id;
 
 const projectsRouter = Router();
 
 projectsRouter.get(projects, getAllProjects);
 projectsRouter.get(myProjects, auth, getUserProjects);
+projectsRouter.delete(`${deleteProject}${id}`, auth, deleteProjects);
 
 export default projectsRouter;
